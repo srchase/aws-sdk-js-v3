889174ebf49567530be2d5ce68e4deb5e9e97ab2
Chase Coalwell <chasecoa@amazon.com>
2019-09-17 09:57:50 -0700

    feat: add deserializers

---
+++ clients/node/client-rds-data-node/protocol/AwsRestJson1_1Deserializers.ts
@@ -0,0 +1,97 @@
+import { Field, ColumnMetadata } from "../models/com/amazon/rdsdataservice";
+
+export function columnMetadataAwsRestJson1_1Deserialize(
+  input: any
+): ColumnMetadata {
+  let columnMetadata: any = {
+    $namespace: "com.amazon.rdsdataservice",
+    $name: "ColumnMetadata"
+  };
+  if (input.nullable) {
+    columnMetadata.nullable = input.nullable;
+  }
+
+  if (input.label) {
+    columnMetadata.label = input.label;
+  }
+
+  if (input.typeName) {
+    columnMetadata.typeName = input.typeName;
+  }
+
+  if (input.isCaseSensitive) {
+    columnMetadata.isCaseSensitive = input.isCaseSensitive;
+  }
+
+  if (input.isCurrency) {
+    columnMetadata.isCurrency = input.isCurrency;
+  }
+
+  if (input.arrayBaseColumnType) {
+    columnMetadata.arrayBaseColumnType = input.arrayBaseColumnType;
+  }
+
+  if (input.precision) {
+    columnMetadata.precision = input.precision;
+  }
+
+  if (input.type) {
+    columnMetadata.type = input.type;
+  }
+
+  if (input.isSigned) {
+    columnMetadata.isSigned = input.isSigned;
+  }
+
+  if (input.schemeName) {
+    columnMetadata.schemeName = input.schemeName;
+  }
+
+  if (input.isAutoIncrement) {
+    columnMetadata.isAutoIncrement = input.isAutoIncrement;
+  }
+
+  if (input.name) {
+    columnMetadata.name = input.name;
+  }
+
+  if (input.scale) {
+    columnMetadata.scale = input.scale;
+  }
+
+  if (input.tableName) {
+    columnMetadata.tableName = input.tableName;
+  }
+
+  return columnMetadata;
+}
+
+export function columnMetadataListAwsRestJson1_1Deserialize(
+  input: any
+): Array<ColumnMetadata> {
+  let list: Array<ColumnMetadata> = [];
+  for (let ColumnMetadata of input) {
+    list.push(columnMetadataAwsRestJson1_1Deserialize(ColumnMetadata));
+  }
+  return input;
+}
+
+export function fieldAwsRestJson1_1Deserialize(input: any): Field {
+  return input;
+}
+
+export function generatedFieldsAwsRestJson1_1Deserialize(
+  input: any
+): Array<Field> {
+  let list: Array<Field> = [];
+  for (let Field of input) {
+    list.push(fieldAwsRestJson1_1Deserialize(Field));
+  }
+  return input;
+}
+
+export function recordsAwsRestJson1_1Deserialize(
+  input: any
+): Array<Array<Field>> {
+  return input;
+}
